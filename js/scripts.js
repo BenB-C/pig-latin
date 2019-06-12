17152a4473172386500e897eccc85eeafffb383a
Ben Bennett-Cauchon and Jess Doss <ben@>
2019-06-12 15:25:20 -0700

    take sentences and turn them into piglatin

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,30 @@ $(document).ready(function() {
     event.preventDefault();
 
     var userInput = $("input#userInput").val();
-    var pigLatinText = igpayAtinlay(userInput);
-
-    $("#result").show();
-    $("#plainText").text(userInput);
-    $("#pigLatinText").text(pigLatinText);
+    if (userInput === '') {
+      $("#error").show();
+      $("#result").hide();
+    } else {
+      var pigLatinText = pigLatinForSentence(userInput);
+      $("#result").show();
+      $("#error").hide();
+      $("#plainText").text(userInput);
+      $("#pigLatinText").text(pigLatinText);
+    }
   });
 });
 
+// Returns the pig latin version of a word. Assumes given userText is not the empty string.
 function igpayAtinlay(userText) {
-  var vowelArray = ['a', 'e', 'i', 'o', 'u'];
-
   var word = userText;
   var firstLetter = word[0];
 
-  if (vowelArray.includes(firstLetter.toLowerCase())) {
+  if (isVowel(firstLetter) && firstLetter.toLowerCase() !== 'y') {
     return word + 'way';
   } else {
     for (var letterPosition = 1; letterPosition < word.length; letterPosition++) {
       var currentLetter = word[letterPosition];
-      if (vowelArray.includes(currentLetter.toLowerCase())) {
+      if (isVowel(currentLetter)) {
         var previousLetter = word[letterPosition - 1];
         if (previousLetter === 'q' && currentLetter === 'u') {
           return word.slice(letterPosition + 1, word.length) + word.slice(0, letterPosition + 1) + 'ay';
@@ -34,4 +38,19 @@ function igpayAtinlay(userText) {
   }
 
   return userText;
+}
+
+function isVowel(letter) {
+  var vowelArray = ['a', 'e', 'i', 'o', 'u', 'y'];
+  return vowelArray.includes(letter.toLowerCase())
+}
+
+// Returns the pig latin for a sentence of words seperated by spaces
+function pigLatinForSentence(sentence) {
+  var words = sentence.split(' ');
+  var pigLatinWordsArray = []
+  for (var wordPosition = 0; wordPosition < words.length; wordPosition++) {
+    pigLatinWordsArray.push(igpayAtinlay(words[wordPosition]));
+  }
+  return pigLatinWordsArray.join(' ');
 }
