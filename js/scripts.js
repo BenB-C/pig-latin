6e64f89f15494753822c342ec9a1357c71f3763c
Ben Bennett-Cauchon and Jess Doss <ben@>
2019-06-12 14:08:10 -0700

    add functionality for single words

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,25 @@ $(document).ready(function() {
 
 function igpayAtinlay(userText) {
   var vowelArray = ['a', 'e', 'i', 'o', 'u'];
-  var firstLetter = userText[0];
-  if (vowelArray.includes(firstLetter.toLowerCase())) {
-    return userText + 'way';
-  }
 
+  var word = userText;
+  var firstLetter = word[0];
+
+  if (vowelArray.includes(firstLetter.toLowerCase())) {
+    return word + 'way';
+  } else {
+    for (var letterPosition = 1; letterPosition < word.length; letterPosition++) {
+      var currentLetter = word[letterPosition];
+      if (vowelArray.includes(currentLetter.toLowerCase())) {
+        var previousLetter = word[letterPosition - 1];
+        if (previousLetter === 'q' && currentLetter === 'u') {
+          return word.slice(letterPosition + 1, word.length) + word.slice(0, letterPosition + 1) + 'ay';
+        }
+        return word.slice(letterPosition, word.length) + word.slice(0, letterPosition) + 'ay';
+      }
+    }
+    return word + 'ay';
+  }
 
   return userText;
 }
